45672e9987e4d93dc392a6171e62863e7d461151
Pictóreo Con Creces <espordemasdeobvioquesoyyo@outlook.com>
2025-04-22 12:26:52 -0300

    feat: Mejora el script de instalación de Termux con mensajes y accesos directos
    - Se añade feedback visual al script `termux-setup.sh` mediante mensajes informativos en la consola durante cada etapa del proceso de instalación.
    - Se modifica el script para crear un acceso directo "Grabar placa" en el directorio `$HOME/.shortcuts` para su uso con Termux-Widget, facilitando la ejecución del script `grabar-placa.sh`.
    - Se eliminan las notificaciones toast, reemplazándolas con mensajes en la consola.
    - Se añaden shebang y flags para mejorar la robustez del script.

--- termux/termux-setup.sh
+++ termux/termux-setup.sh
@@ -1,45 +1,60 @@
-# Script para ejecutar en android
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "\n====================================="
+echo "   🐔  LIBRE GALLINERO SETUP 🐔  "
+echo "====================================="
+echo
+
+set -e  # Hacer que el script falle si hay un error
+set -u  # Hacer que el script falle si se usa una variable no definida
+set -o pipefail  # Hacer que el script falle si un comando en una tubería falla
 
 # 0. Iniciar
 cd $HOME
 
 # 1. Instalar las dependencias necesarias
+echo "\n\n[1] Instalando dependencias necesarias..."
 pkg update -y
 pkg install -y root-repo
 pkg upgrade -y
 pkg install -y git python python-pip termux-api termux-tools 
 
 # 2. Clonar el repositorio libre-gallinero (si no existe)
+echo "\n\n[2] Clonando el repositorio libre-gallinero (si no existe)..."
 if [ ! -d "libre-gallinero" ]; then
   git clone https://github.com/pwqw/libre-gallinero.git
 fi
 
 # 3. Navegar al directorio del repositorio
+echo "\n\n[3] Navegando al directorio del repositorio..."
 cd libre-gallinero
 
 # 4. Actualizar el repositorio (forzado)
+echo "\n\n[4] Actualizando el repositorio (forzado)..."
 git fetch --all
 git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
 
 # 5. Crear el entorno virtual (si no existe)
+echo "\n\n[5] Creando el entorno virtual (si no existe)..."
 if [ ! -d "env" ]; then
   python -m venv env
 fi
 
 # 6. Activar el entorno virtual
+echo "\n\n[6] Activando el entorno virtual..."
 source env/bin/activate
 
 # 7. Instalar las dependencias del proyecto
+echo "\n\n[7] Instalando dependencias del proyecto..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# 8. Notificación de éxito
-termux-toast "Entorno listo para libre-gallinero"
-
-# 9. Crear el acceso directo del script grabar-placa.sh para Termux-Widget
+# 8. Crear el acceso directo del script grabar-placa.sh para Termux-Widget
+echo "\n\n[8] Creando acceso directo para Termux-Widget..."
 if [ ! -d "$HOME/.shortcuts" ]; then
   mkdir -p "$HOME/.shortcuts"
 fi
 cp -f $HOME/libre-gallinero/grabar-placa.sh "$HOME/.shortcuts/Grabar placa"
 chmod +x "$HOME/.shortcuts/Grabar placa"
+echo "\n\n🐔  ¡Listo! Puedes usar el widget 'Grabar placa' en Termux-Widget. 🐔\n"
 
